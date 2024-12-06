ac11092ff98c6d4045f50b4b4a97cdf05188c397
AlexZ-WP <alexz10@mail.ru>
2024-12-06 16:27:06 +0300

    Add files via upload

--- module_7_3.py
+++ module_7_3.py
@@ -2,31 +2,45 @@ import string
 
 
 class WordsFinder:
-
     def __init__(self, *file_names):
-        pass
-       #self.file_names = file_names[]
+        self.file_names = file_names
 
     def get_all_words(self):
-        all_words ={}
+        all_words = {}
         for i in self.file_names:
-            with open(self.file_names) as file:
-                line_l = []
-                for line in file:
-                    table = str.maketrans("", "", string.punctuation)
-                    new_line = line.translate(table)
-                    new_line = line.lower()
-                    new_line.split()
-                    line_l = new_line.split()
-            for key in self.file_names:
-                for value in line_l:
-                    all_words[key] = value
-                    break
+            with open(i, 'r') as file:
+                line_l = []  # режим открытия можно и не указывать
+                line = file.read()
+                table = str.maketrans("", "", string.punctuation)
+                #line = line.translate(table) # если используем данную переменную, то убираются все
+                # знаки кроме букв "its"
+                new_line = line.translate(table) # при такой переменной апостроф не убирается "it's"
+                new_line = line.lower()
+                line_l.append(new_line.split())
 
-    def find(self, word):
-        find_d = {}
-        for name, words in get_all_words().items():
-        pass
+                for key in self.file_names:
+                    for value in line_l:
+                        all_words[key] = value
 
-    def count(self, word):
-        pass
+        """Вариант для разбора, выводит или первую строку файла если используем break
+        или последнюю строку, если не используем break
+        """
+        # for i in self.file_names:
+        #     with open(i, 'r') as file:
+        #         line_l = []
+        #         for line in file:
+        #             table = str.maketrans("", "", string.punctuation)
+        #             new_line = line.translate(table)
+        #             new_line = new_line.lower()
+        #             line_l.append(new_line.split())
+        #             # break
+        #         for key in self.file_names:
+        #             for value in line_l:
+        #                 all_words[key] = value
+        #                 break
+        file.close()
+        return all_words
+
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())  # Все слова
