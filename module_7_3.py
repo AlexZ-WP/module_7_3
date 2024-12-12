b3106b3e07909669f768ecff8733751f924cdaba
AlexZ-WP <alexz10@mail.ru>
2024-12-12 21:35:10 +0300

    Add files via upload

--- module_7_3.py
+++ module_7_3.py
@@ -8,39 +8,49 @@ class WordsFinder:
     def get_all_words(self):
         all_words = {}
         for i in self.file_names:
-            with open(i, 'r') as file:
-                line_l = []  # режим открытия можно и не указывать
+            with open(i, 'r', encoding="utf-8") as file:
                 line = file.read()
-                table = str.maketrans("", "", string.punctuation)
-                #line = line.translate(table) # если используем данную переменную, то убираются все
-                # знаки кроме букв "its"
-                new_line = line.translate(table) # при такой переменной апостроф не убирается "it's"
+                table = str.maketrans("", "", ',.=!?;:-')
+                line = line.translate(table)
                 new_line = line.lower()
-                line_l.append(new_line.split())
+                all_words[i] = new_line.split()
 
-                for key in self.file_names:
-                    for value in line_l:
-                        all_words[key] = value
-
-        """Вариант для разбора, выводит или первую строку файла если используем break
-        или последнюю строку, если не используем break
-        """
-        # for i in self.file_names:
-        #     with open(i, 'r') as file:
-        #         line_l = []
-        #         for line in file:
-        #             table = str.maketrans("", "", string.punctuation)
-        #             new_line = line.translate(table)
-        #             new_line = new_line.lower()
-        #             line_l.append(new_line.split())
-        #             # break
-        #         for key in self.file_names:
-        #             for value in line_l:
-        #                 all_words[key] = value
-        #                 break
-        file.close()
         return all_words
+
+    def find(self, word):
+        """
+        Возвращает словарь, где ключ - название файла, значение - позиция первого
+        такого слова в списке слов этого файла.
+        """
+        word_list = {}
+        word = word.lower()
+        #counter = 0
+        for file_names, values in self.get_all_words().items():
+            counter = 0
+            for val in values:
+                counter += 1
+                if word == val.lower():
+                    word_list[file_names] = counter
+                    break
+        return word_list
+
+    def count(self, word):
+        """
+        Возвращает словарь, где ключ - название файла, значение - количество слова word в списке слов этого файла.
+        """
+        word_count = {}
+        word = word.lower()
+        for file_names, values in self.get_all_words().items():
+            counter = 0
+            for val in values:
+                if word == val.lower():
+                    counter += 1
+            word_count[file_names] = counter
+
+        return word_count
 
 
 finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
